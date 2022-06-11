1c79ce03fdc40e4a061fca3b71cdd3206479f207
Alabhya Jindal <alabhya10@gmail.com>
2022-06-11 23:23:11 +0530

    Testing GitHub Linguist

--- index.tsx
+++ index.tsx
@@ -1,4 +1,37 @@
 import { createRoot } from 'react-dom/client';
 const root = createRoot(document.getElementById('root'));
 
-root.render(<h1>Thinking of a project</h1>);
+root.render(
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam est alias
+      accusamus excepturi odit voluptatibus dolores, earum recusandae
+      consequuntur a vitae harum quidem dolor sapiente praesentium quod eligendi
+      unde fugit.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusamus
+      porro neque voluptatem, suscipit error praesentium illum, incidunt autem
+      nisi perspiciatis nostrum repellat cupiditate harum quisquam saepe sunt
+      delectus. Labore!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi,
+      asperiores! Id voluptate aspernatur incidunt sint fuga reiciendis neque ex
+      tenetur ab, ad voluptatem tempore dolor, ipsa, eaque deserunt dolorum
+      animi!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi,
+      asperiores! Id voluptate aspernatur incidunt sint fuga reiciendis neque ex
+      tenetur ab, ad voluptatem tempore dolor, ipsa, eaque deserunt dolorum
+      animi!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi,
+      asperiores! Id voluptate aspernatur incidunt sint fuga reiciendis neque ex
+      tenetur ab, ad voluptatem tempore dolor, ipsa, eaque deserunt dolorum
+      animi!
+    </p>
+  </div>
+);
