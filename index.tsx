96ac5a26be84f6dea25b37b53061921469e89e9f
Alabhya Jindal <alabhya10@gmail.com>
2022-06-11 23:24:22 +0530

    Added README.md

--- index.tsx
+++ index.tsx
@@ -9,29 +9,5 @@ root.render(
       consequuntur a vitae harum quidem dolor sapiente praesentium quod eligendi
       unde fugit.
     </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusamus
-      porro neque voluptatem, suscipit error praesentium illum, incidunt autem
-      nisi perspiciatis nostrum repellat cupiditate harum quisquam saepe sunt
-      delectus. Labore!
-    </p>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi,
-      asperiores! Id voluptate aspernatur incidunt sint fuga reiciendis neque ex
-      tenetur ab, ad voluptatem tempore dolor, ipsa, eaque deserunt dolorum
-      animi!
-    </p>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi,
-      asperiores! Id voluptate aspernatur incidunt sint fuga reiciendis neque ex
-      tenetur ab, ad voluptatem tempore dolor, ipsa, eaque deserunt dolorum
-      animi!
-    </p>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi,
-      asperiores! Id voluptate aspernatur incidunt sint fuga reiciendis neque ex
-      tenetur ab, ad voluptatem tempore dolor, ipsa, eaque deserunt dolorum
-      animi!
-    </p>
   </div>
 );
